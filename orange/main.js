e1997e0116714a72eef57820ef0700b4f43d56fa
Brandon Rimes <77939170+BrandonRimes@users.noreply.github.com>
2022-01-19 21:15:16 -0500

    threejs scene setup

---
+++ orange/main.js
@@ -0,0 +1,17 @@
+import * as THREE from 'three';
+
+import './style.css';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#theCanvas')
+})
+const sphereGeo = new THREE.SphereGeometry(10, 32, 16);
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
